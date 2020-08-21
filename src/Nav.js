d579872354d42994bc67ec3f9f63f8c43fe61055
Justin Muench <55668563+muench-develops@users.noreply.github.com>
2020-08-21 22:52:59 +0200

    Updated

--- src/Nav.js
+++ src/Nav.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import './nav.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSearch,
+  faGift,
+  faBell,
+  faLock,
+} from '@fortawesome/free-solid-svg-icons';
 
 function Nav() {
   return (
     <div className='navbar'>
       <div className='nav-left'>
-        <span class='navBrand'>Netflix</span>
-        <span>Home</span>
-        <span>TV Shows</span>
-        <span>Movies</span>
-        <span>Latest</span>
+        <span className='navBrand'>Netflix</span>
+        <span className='nav-item'>Home</span>
+        <span className='nav-item'>TV Shows</span>
+        <span className='nav-item'>Movies</span>
+        <span className='nav-item'>Latest</span>
       </div>
       <div className='nav-right'>
-        <span>bla</span>
-        <span>bla</span>
-        <span>bla</span>
+        <span className='nav-item'>
+          <FontAwesomeIcon className='nav-item-icon' icon={faSearch} />
+        </span>
+        <span className='nav-item'>kids</span>
+        <span className='nav-item'>
+          <FontAwesomeIcon className='nav-item-icon' icon={faGift} />
+        </span>
+        <span className='nav-item'>
+          <FontAwesomeIcon className='nav-item-icon' icon={faBell} />
+        </span>
+        <span className='nav-item'>
+          <FontAwesomeIcon className='nav-item-icon' icon={faLock} />
+        </span>
       </div>
     </div>
   );
